3c40e4f49d15d923a5eb207064c4cf35bafdc610
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2012-11-21 00:09:55 +0200

    Left-over code re-formatting.

--- src/toolbox/design/patterns/structural/facade/HardDrive.java
+++ src/toolbox/design/patterns/structural/facade/HardDrive.java
@@ -1,27 +1,22 @@
-/**
- * 
- */
 package toolbox.design.patterns.structural.facade;
 
 /**
- * Subsystem class. Implement subsystem functionality having no knowledge
- * of the facade.
- * 
+ * Subsystem class. Implement subsystem functionality having no knowledge of the
+ * facade.
+ *
  * @author billy
  *
  */
 public class HardDrive {
 
-	/**
-	 * 
-	 */
-	public HardDrive() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public byte[] read(long lba, int size) {
-		System.out.println("Reading "+ size + " bytes from logical block address 0x" + Long.toHexString(lba) + ".");// TODO Method stub
-		return null;
-	}
+    public HardDrive() {
+    }
+
+    public byte[] read(long lba, int size) {
+        System.out.println("Reading " + size
+                + " bytes from logical block address 0x"
+                + Long.toHexString(lba) + ".");// TODO Method stub
+        return null;
+    }
 
 }
