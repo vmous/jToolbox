6e78c1bf0d5bad0c2544897e76dd283971f69b55
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2013-01-22 17:36:37 +0200

    Unit test for the Iterator behavioral design pattern.

---
+++ test/toolbox/design/patterns/behavioral/iterator/IteratorTest.java
@@ -0,0 +1,52 @@
+package toolbox.design.patterns.behavioral.iterator;
+
+import org.junit.After;
+import org.junit.Before;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for the Iterator behavioral design pattern.
+ *
+ * @author billy
+ */
+public class IteratorTest extends TestCase {
+
+    private final String[] books = {
+            "Design Patterns",
+            "Tractatus Logico-Philosophicus",
+            "Modern Operating Systems",
+            "Principia Mathematica"
+            };
+
+    private Iterator<String> itr;
+
+    private BookList bookList;
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        bookList = new BookList(books);
+        itr = bookList.createIterator();
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Override
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    public void testIt() {
+        int i = 0;
+
+        while (!itr.isDone()) {
+            assertEquals(books[i++], itr.next());
+        }
+    }
+
+}
