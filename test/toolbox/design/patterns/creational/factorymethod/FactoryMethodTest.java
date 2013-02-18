dad0cb71b44d1e7ff9ae8b409a175dc2edae1a44
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2013-02-19 00:02:16 +0200

    Unit test for the Factory Method creational design pattern.

---
+++ test/toolbox/design/patterns/creational/factorymethod/FactoryMethodTest.java
@@ -0,0 +1,39 @@
+package toolbox.design.patterns.creational.factorymethod;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import toolbox.design.patterns.creational.factorymethod.UserAuthenticationFactory.AuthenticationType;
+
+/**
+ * Unit test for the Factory Method creational design pattern.
+ *
+ * @author billy
+ */
+public class FactoryMethodTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testIt() {
+		UserAuthenticationFactory factory = new UserAuthenticationFactory();
+        UserAuthentication authOpenLdap = factory.getUserAuthenication(AuthenticationType.OPEN_LDAP);
+		UserAuthentication authActiveDirectory = factory.getUserAuthenication(AuthenticationType.ACTIVE_DIRECTORY);
+
+		// Simulates that the authentication against an Active Directory fails.
+		assertFalse(authActiveDirectory.authenticate());
+
+		// Simulates that the authentication against an Open LDAP server succeeds.
+		assertTrue(authOpenLdap.authenticate());
+    }
+
+}
