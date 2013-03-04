87eec0b6c356decd41d8dbb7665210ade9988f03
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2013-03-04 23:46:42 +0200

    Some additional array brainers: "commonEnd", "reverse3", "middleWay", "no23", "fix23", "makeMiddle", "midThree", "unlucky1", "sameFirstLast".

--- src/toolbox/brainer/Arrays.java
+++ src/toolbox/brainer/Arrays.java
@@ -1,7 +1,7 @@
 package toolbox.brainer;
 
 /**
- * Array problems.
+ * Array brainers.
  *
  * @author billy
  */
@@ -49,6 +49,194 @@ public class Arrays {
             return b;
         else
             return a;
+    }
+
+
+    /**
+     * Given 2 arrays of ints, a and b, return true if they have the same first
+     * element or they have the same last element. Both arrays will be length 1
+     * or more.
+     *
+     * @param a
+     *     The first array.
+     * @param b
+     *     The second array.
+     *
+     * @return
+     *     {@code true} if they have their first or last elements equal;
+     *     {@code false} otherwise.
+     */
+    public static boolean commonEnd(int[] a, int[] b) {
+        boolean is = false;
+
+        if (a.length > 0 && b.length > 0)
+            if ( (a[0] == b[0]) || (a[a.length - 1] == b[b.length - 1]) )
+                is = true;
+
+        return is;
+    }
+
+
+    /**
+     * Given an array of ints length 3, return a new array with the elements
+     * in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+     *
+     * @param nums
+     *     The array.
+     *
+     * @return
+     *     The given array with its elements in reverse order.
+     */
+    public static int[] reverse3(int[] nums) {
+        int[] newNums = new int[3];
+        newNums[0] = nums[2];
+        newNums[1] = nums[1];
+        newNums[2] = nums[0];
+
+        return newNums;
+    }
+
+
+    /**
+     * Given 2 int arrays, a and b, each length 3, return a new array length 2
+     * containing their middle elements.
+     *
+     * @param a
+     *     The first array.
+     * @param b
+     *     The second array.
+     *
+     * @return
+     *     A two-element array containing the given arrays' middle elements.
+     */
+    public static int[] middleWay(int[] a, int[] b) {
+        int[] newArray = new int[2];
+
+        newArray[0] = a[1];
+        newArray[1] = b[1];
+
+        return newArray;
+    }
+
+
+    /**
+     * Given an int array length 2, return true if it does not contain a 2 or 3.
+     *
+     * @param nums
+     *     The array.
+     *
+     * @return
+     *     {@code true} if it does not contain a 2 or 3; {@code false}
+     *     otherwise.
+     */
+    public static boolean no23(int[] nums) {
+        return ( (nums[0] != 2 && nums[0] != 3 && nums[1] != 2 && nums[1] != 3) );
+    }
+
+
+    /**
+     * Given an int array length 3, if there is a 2 in the array immediately
+     * followed by a 3, set the 3 element to 0. Return the changed array.
+     *
+     * @param nums
+     *     The array.
+     *
+     * @return
+     *     Returns the same array any 3 following a 2, changed to 0.
+     */
+    public static int[] fix23(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == 2)
+                if( (i < nums.length - 1) && nums[i + 1] == 3)
+                    nums[i + 1] = 0;
+        }
+        return nums;
+    }
+
+
+    /**
+     * Given an array of ints of even length, return a new array length 2
+     * containing the middle two elements from the original array. The original
+     * array will be length 2 or more.
+     *
+     * @param nums
+     *     The array of even length (at least 2).
+     *
+     * @return
+     *     A new array of length 2 containing the middle elements of the given
+     *     array.
+     */
+    public static int[] makeMiddle(int[] nums) {
+        int[] newArray = new int[2];
+
+        newArray[0] = nums[(int) Math.floor((double) (nums.length - 1) / 2)];
+        newArray[1] = nums[(int) Math.ceil((double) (nums.length - 1) / 2)];
+
+        return newArray;
+    }
+
+
+    /**
+     * Given an array of ints of odd length, return a new array length 3
+     * containing the elements from the middle of the array. The array length
+     * will be at least 3.
+     *
+     * @param nums
+     *     The array of odd lenght (at least 3).
+     *
+     * @return
+     *     A new array of length 3 containing the middle elements of given
+     *     array.
+     */
+    public static int[] midThree(int[] nums) {
+        int[] newArray = new int[3];
+        int mid = (nums.length - 1) / 2;
+
+        newArray[0] = nums[mid - 1];
+        newArray[1] = nums[mid];
+        newArray[2] = nums[mid + 1];
+
+        return newArray;
+    }
+
+
+    /**
+     * We'll say that a 1 immediately followed by a 3 in an array is an
+     * "unlucky" 1. Return true if the given array contains an unlucky 1 in the
+     * first 2 or last 2 positions in the array.
+     *
+     * @param nums
+     *     The array.
+     *
+     * @return
+     *     {@code true} if the given array contains an unlucky 1 in the first
+     *     2 or last 2 positions in the array.
+     */
+    public static boolean unlucky1(int[] nums) {
+        boolean is = false;
+
+        for (int i = 0; i < nums.length; i++)
+            if (nums[i] == 1 && (i < 2 || i == nums.length - 2))
+                if (i < nums.length - 1 && nums[i + 1] == 3)
+                    is = true;
+
+        return is;
+    }
+
+
+    /**
+     * Given an array of ints, return true if the array is length 1 or more, and
+     * the first element and the last element are equal.
+     *
+     * @param nums
+     *     The array.
+     *
+     * @return
+     *     {@code true} if the is lenght 1 or more and the first element and the
+     *     last are equal.
+     */
+    public static boolean sameFirstLast(int[] nums) {
+        return ( nums.length > 0 && (nums[0] == nums[nums.length - 1]) );
     }
 
 }
--- test/toolbox/brainer/ArraysTest.java
+++ test/toolbox/brainer/ArraysTest.java
@@ -52,4 +52,121 @@ public class ArraysTest {
         assertArrayEquals(new int[]{6, 7}, Arrays.biggerTwo(new int[]{6, 7}, new int[]{3, 1}));
     }
 
+
+    @Test
+    public void testCommonEnd() {
+        assertTrue(Arrays.commonEnd(new int[]{1, 2, 3}, new int[]{7, 3}));
+        assertFalse(Arrays.commonEnd(new int[]{1, 2, 3}, new int[]{7, 3, 2}));
+        assertTrue(Arrays.commonEnd(new int[]{1, 2, 3}, new int[]{1, 3}));
+        assertTrue(Arrays.commonEnd(new int[]{1, 2, 3}, new int[]{1}));
+        assertFalse(Arrays.commonEnd(new int[]{1, 2, 3}, new int[]{2}));
+    }
+
+
+    @Test
+    public void testReverse3() {
+        assertArrayEquals(new int[]{3, 2, 1}, Arrays.reverse3(new int[]{1, 2, 3}));
+        assertArrayEquals(new int[]{9, 11, 5}, Arrays.reverse3(new int[]{5, 11, 9}));
+        assertArrayEquals(new int[]{0, 0, 7}, Arrays.reverse3(new int[]{7, 0, 0}));
+        assertArrayEquals(new int[]{2, 1, 2}, Arrays.reverse3(new int[]{2, 1, 2}));
+        assertArrayEquals(new int[]{1, 2, 1}, Arrays.reverse3(new int[]{1, 2, 1}));
+        assertArrayEquals(new int[]{3, 11, 2}, Arrays.reverse3(new int[]{2, 11, 3}));
+        assertArrayEquals(new int[]{5, 6, 0}, Arrays.reverse3(new int[]{0, 6, 5}));
+        assertArrayEquals(new int[]{3, 2, 7}, Arrays.reverse3(new int[]{7, 2, 3}));
+    }
+
+
+    @Test
+    public void testMiddleWay() {
+        assertArrayEquals(new int[]{2, 5}, Arrays.middleWay(new int[]{1, 2, 3}, new int[]{4, 5, 6}));
+        assertArrayEquals(new int[]{7, 8}, Arrays.middleWay(new int[]{7, 7, 7}, new int[]{3, 8, 0}));
+        assertArrayEquals(new int[]{2, 4}, Arrays.middleWay(new int[]{5, 2, 9}, new int[]{1, 4, 5}));
+        assertArrayEquals(new int[]{9, 8}, Arrays.middleWay(new int[]{1, 9, 7}, new int[]{4, 8, 8}));
+        assertArrayEquals(new int[]{2, 1}, Arrays.middleWay(new int[]{1, 2, 3}, new int[]{3, 1, 4}));
+        assertArrayEquals(new int[]{2, 1}, Arrays.middleWay(new int[]{1, 2, 3}, new int[]{4, 1, 1}));
+    }
+
+
+    @Test
+    public void testNo23() {
+        assertTrue(Arrays.no23(new int[]{4, 5}));
+        assertFalse(Arrays.no23(new int[]{4, 2}));
+        assertFalse(Arrays.no23(new int[]{3, 5}));
+        assertTrue(Arrays.no23(new int[]{1, 9}));
+        assertFalse(Arrays.no23(new int[]{2, 9}));
+        assertFalse(Arrays.no23(new int[]{1, 3}));
+        assertTrue(Arrays.no23(new int[]{1, 1}));
+        assertFalse(Arrays.no23(new int[]{2, 2}));
+        assertFalse(Arrays.no23(new int[]{3, 3}));
+        assertTrue(Arrays.no23(new int[]{7, 8}));
+        assertTrue(Arrays.no23(new int[]{8, 7}));
+    }
+
+
+    @Test
+    public void testFix23() {
+        assertArrayEquals(new int[]{1, 2, 0}, Arrays.fix23(new int[]{1, 2, 3}));
+        assertArrayEquals(new int[]{2, 0, 5}, Arrays.fix23(new int[]{2, 3, 5}));
+        assertArrayEquals(new int[]{1, 2, 1}, Arrays.fix23(new int[]{1, 2, 1}));
+        assertArrayEquals(new int[]{3, 2, 1}, Arrays.fix23(new int[]{3, 2, 1}));
+        assertArrayEquals(new int[]{2, 2, 0}, Arrays.fix23(new int[]{2, 2, 3}));
+        assertArrayEquals(new int[]{2, 0, 3}, Arrays.fix23(new int[]{2, 3, 3}));
+    }
+
+
+    @Test
+    public void testMakeMiddle() {
+        assertArrayEquals(new int[]{2, 3}, Arrays.makeMiddle(new int[]{1, 2, 3, 4}));
+        assertArrayEquals(new int[]{2, 3}, Arrays.makeMiddle(new int[]{7, 1, 2, 3, 4, 9}));
+        assertArrayEquals(new int[]{1, 2}, Arrays.makeMiddle(new int[]{1, 2}));
+        assertArrayEquals(new int[]{2, 4}, Arrays.makeMiddle(new int[]{5, 2, 4, 7}));
+        assertArrayEquals(new int[]{4, 3}, Arrays.makeMiddle(new int[]{9, 0, 4, 3, 9, 1}));
+    }
+
+
+    @Test
+    public void testMidThree() {
+        assertArrayEquals(new int[]{2, 3, 4}, Arrays.midThree(new int[]{1, 2, 3, 4, 5}));
+        assertArrayEquals(new int[]{7, 5, 3}, Arrays.midThree(new int[]{8, 6, 7, 5, 3, 0, 9}));
+        assertArrayEquals(new int[]{1, 2, 3}, Arrays.midThree(new int[]{1, 2, 3}));
+    }
+
+
+    @Test
+    public void testUnlucky1() {
+        assertTrue(Arrays.unlucky1(new int[]{1, 3, 4, 5}));
+        assertTrue(Arrays.unlucky1(new int[]{2, 1, 3, 4, 5}));
+        assertFalse(Arrays.unlucky1(new int[]{1, 1, 1}));
+        assertTrue(Arrays.unlucky1(new int[]{1, 3, 1}));
+        assertTrue(Arrays.unlucky1(new int[]{1, 1, 3}));
+        assertFalse(Arrays.unlucky1(new int[]{1, 2, 3}));
+        assertFalse(Arrays.unlucky1(new int[]{3, 3, 3}));
+        assertTrue(Arrays.unlucky1(new int[]{1, 3}));
+        assertFalse(Arrays.unlucky1(new int[]{1, 4}));
+        assertFalse(Arrays.unlucky1(new int[]{1}));
+        assertFalse(Arrays.unlucky1(new int[]{}));
+        assertFalse(Arrays.unlucky1(new int[]{1, 1, 1, 3, 1}));
+        assertTrue(Arrays.unlucky1(new int[]{1, 1, 3, 1, 1}));
+        assertTrue(Arrays.unlucky1(new int[]{1, 1, 1, 1, 3}));
+        assertFalse(Arrays.unlucky1(new int[]{1, 4, 1, 5}));
+        assertFalse(Arrays.unlucky1(new int[]{1, 1, 2, 3}));
+        assertFalse(Arrays.unlucky1(new int[]{2, 3, 2, 1}));
+        assertTrue(Arrays.unlucky1(new int[]{2, 3, 1, 3}));
+        assertTrue(Arrays.unlucky1(new int[]{1, 2, 3, 4, 1, 3}));
+    }
+
+
+    @Test
+    public void testSameFirstLast() {
+        assertFalse(Arrays.sameFirstLast(new int[]{1, 2, 3}));
+        assertTrue(Arrays.sameFirstLast(new int[]{1, 2, 3, 1}));
+        assertTrue(Arrays.sameFirstLast(new int[]{1, 2, 1}));
+        assertTrue(Arrays.sameFirstLast(new int[]{7}));
+        assertFalse(Arrays.sameFirstLast(new int[]{}));
+        assertTrue(Arrays.sameFirstLast(new int[]{1, 2, 3, 4, 5, 1}));
+        assertFalse(Arrays.sameFirstLast(new int[]{1, 2, 3, 4, 5, 13}));
+        assertTrue(Arrays.sameFirstLast(new int[]{13, 2, 3, 4, 5, 13}));
+        assertTrue(Arrays.sameFirstLast(new int[]{7, 7}));
+    }
+
 }
